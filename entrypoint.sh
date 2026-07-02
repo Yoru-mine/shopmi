fc4cdd075e17e92a203fa0502e300cd9f4b978d7
Myavk <lyunyatut@gmail.com>
2026-07-02 09:08:07 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# Выполняем миграции
+echo "--- Начинаю миграции ---"
 php artisan migrate --force
 
-# Запускаем сидеры (если нужно)
-# php artisan db:seed --force
+echo "--- Начинаю сидинг ---"
+php artisan db:seed --force
 
-# Запускаем основной процесс (PHP)
+echo "--- Запускаю сервер ---"
 php artisan serve --host=0.0.0.0 --port=8000
